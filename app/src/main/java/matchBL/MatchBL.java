9c9740d8af9d2015a4cf3d9a85dff699e29cc089
yd1996 <834269940@qq.com>
2015-04-13 16:08:06 +0800

    现在可以放心大胆的同步了

--- app/src/main/java/matchBL/MatchBL.java
+++ app/src/main/java/matchBL/MatchBL.java
@@ -2,6 +2,7 @@ package matchBL;
 
 import java.util.ArrayList;
 
+import po.ContactPO;
 import po.MatchInfoPO;
 
 /**
@@ -9,7 +10,33 @@ import po.MatchInfoPO;
  */
 public class MatchBL {
 
+    public static ArrayList<ContactPO> data ;
+
+    public static void setContactList(ArrayList<ContactPO> newData){
+        data = newData;
+    }
+
     public static ArrayList<MatchInfoPO> getMatchInfoList(){
+        return null;
+    }
+
+    public static ArrayList<CheckedImg> getCheckerImgList(){
+        ArrayList<CheckedImg> result = new ArrayList<CheckedImg>();
+        if(data == null||data.size() == 0){
+            return result;
+        }
+        else{
+            for(int i = 0;i <= data.size() - 1;i++){
+                CheckedImg img = new CheckedImg();
+                img.setImg(data.get(i).getImageurl());
+                img.setId(String.valueOf(i));
+                img.setName(data.get(i).getName());
+            }
+            return result;
+        }
+    }
+
+    public static MatchInfoPO getMatchResult(int[] position){
         return null;
     }
 }
