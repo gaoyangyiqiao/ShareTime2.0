6fc66d49827037271ac0d2ac5adf9c99c97000a4
zfy <1696621890@qq.com>
2015-04-30 00:33:59 +0800

    k

---
+++ app/src/main/java/turingmachine/com/sharetime20/SchedulePre.java
@@ -0,0 +1,13 @@
+package turingmachine.com.sharetime20;
+
+import po.SchedulePO;
+
+/**
+ * Created by hello on 2015/4/29.
+ */
+public class SchedulePre {
+    private SchedulePO schedule;
+    public SchedulePre(SchedulePO schedule){
+        this.schedule=schedule;
+    }
+}
