3ab7957c2b45e0cd64e616508e382b36390cb18e
gaoyangyiqiao <gyyq1994@gmail.com>
2015-05-03 00:11:54 +0800

    net

--- app/src/main/java/netconnection/Match.java
+++ app/src/main/java/netconnection/Match.java
@@ -10,6 +10,7 @@ import org.json.JSONObject;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import po.ActivityPO;
 import po.SchedulePO;
@@ -40,14 +41,26 @@ public class Match {
                     JSONArray activities=userSchedule.getJSONArray(Config.KEY_ACTIVITY);
                     for(int i=0;i<activities.length();i++){
                         JSONObject json_activity=activities.getJSONObject(i);
+                        String theme=json_activity.getString(Config.KEY_THEME);
+                        String founder=json_activity.getString(Config.KEY_FOUNDER_ID);
                         String content=json_activity.getString(Config.KEY_CONTENT);
                         String id=json_activity.getString(Config.KEY_ID);
                         String contacts_id=json_activity.getString(Config.KEY_CONTACTS_ID);
-//                        ActivityPO activity=new ActivityPO(content,id,contacts_id);
-//                        schedule.getActivityList().add(activity);
+                        String receive_time_str=json_activity.getString(Config.KEY_RECEIVE_TIME);
+                        String start_time_str=json_activity.getString(Config.KEY_START_TIME);
+                        String end_time_str=json_activity.getString(Config.KEY_END_TIME);
+                        SimpleDateFormat sdf=new SimpleDateFormat(Config.DATE_PATTERN);
+                        Date receive_time=sdf.parse(receive_time_str);
+                        Date start_time=sdf.parse(start_time_str);
+                        Date end_time=sdf.parse(end_time_str);
+                        int right=json_activity.getInt(Config.KEY_RIGHT);
+
+                        ActivityPO activity=new ActivityPO(id,theme,content,founder,receive_time,
+                                start_time,end_time,contacts_id,right);
+                        schedule.getActivityList().add(activity);
                     }
 
-                    SimpleDateFormat dateFormat=new SimpleDateFormat("yyyy-mm-dd hh:MM");
+                    SimpleDateFormat dateFormat=new SimpleDateFormat(Config.DATE_PATTERN);
                     schedule.setStartTime(dateFormat.parse(userSchedule.getString(Config.KEY_BEGIN_TIME)));
                     schedule.setLength(userSchedule.getInt(Config.KEY_SIZE));
 
