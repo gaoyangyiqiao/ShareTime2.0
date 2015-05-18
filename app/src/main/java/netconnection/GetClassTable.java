4da452bc3937e41a3f14ae76ca379dc19df966f0
gaoyangyiqiao <gyyq1994@gmail.com>
2015-05-19 00:26:40 +0800

    。

---
+++ app/src/main/java/netconnection/GetClassTable.java
@@ -0,0 +1,14 @@
+package netconnection;
+
+import turingmachine.com.sharetime20.ScheduleView;
+
+/**
+ * Created by gaoyang on 15/5/18.
+ */
+public class GetClassTable {
+    public GetClassTable(String userId,final ScheduleView scheduleView){
+
+    }
+
+
+}
