8758de380d946e596ea97ad3be337ad8cae569a5
zfy <1696621890@qq.com>
2015-05-30 21:39:34 +0800

    k

---
+++ app/src/main/java/weekview/DateTimeInterpreter.java
@@ -0,0 +1,14 @@
+package weekview;
+
+/**
+ * Created by hello on 2015/5/30.
+ */
+import java.util.Calendar;
+
+/**
+ * Created by Raquib on 1/6/2015.
+ */
+public interface DateTimeInterpreter {
+    String interpretDate(Calendar date);
+    String interpretTime(int hour);
+}
