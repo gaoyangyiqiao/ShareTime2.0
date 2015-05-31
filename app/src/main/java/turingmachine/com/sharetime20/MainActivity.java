438f9c01d205024c7f4cccaaaba3571b73c12972
gaoyangyiqiao <gyyq1994@gmail.com>
2015-05-31 09:33:08 +0800

    something

--- app/src/main/java/turingmachine/com/sharetime20/MainActivity.java
+++ app/src/main/java/turingmachine/com/sharetime20/MainActivity.java
@@ -11,6 +11,7 @@ import android.view.View;
 import android.view.animation.AnimationUtils;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.view.View.OnClickListener;
