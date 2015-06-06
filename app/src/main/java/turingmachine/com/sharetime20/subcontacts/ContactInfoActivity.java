57cbb814c40825bcb2be0e960e2278d92f4f19a5
gaoyangyiqiao <gyyq1994@gmail.com>
2015-06-06 23:19:32 +0800

    TEST3

--- app/src/main/java/turingmachine/com/sharetime20/subcontacts/ContactInfoActivity.java
+++ app/src/main/java/turingmachine/com/sharetime20/subcontacts/ContactInfoActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -17,6 +18,7 @@ public class ContactInfoActivity extends Activity {
     private TextView name,account,tip;
     private Button deleteBtn;
     private int id;
+    private TextView tv_back;
     private ContactsInfoController contactsInfoController;
 
     @Override
@@ -25,15 +27,8 @@ public class ContactInfoActivity extends Activity {
         setContentView(R.layout.activity_contact_info);
 
         contactsInfoController=new ContactsInfoController();
-        //初始化
-        icon= (ImageView) findViewById(R.id.contact_info_img);
-        name= (TextView) findViewById(R.id.contact_info_name);
-        account= (TextView) findViewById(R.id.contact_info_account);
-        tip= (TextView) findViewById(R.id.contact_info_tv_tip);
-        deleteBtn= (Button) findViewById(R.id.contact_info_btn_delete);
-
+        initViews();
         id=getIntent().getIntExtra("id",-1);
-
         if(id!=-1){
             contactsInfoController.displayContactInfo(id+"",this);
         }
@@ -42,28 +37,20 @@ public class ContactInfoActivity extends Activity {
         //左上角返回按钮
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_contact_info, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch(item.getItemId()){
-            case R.id.action_settings:
-                break;
-            //这里的id是主activity的id
-            case R.id.home:
+    public void initViews(){
+        //初始化
+        icon= (ImageView) findViewById(R.id.contact_info_img);
+        name= (TextView) findViewById(R.id.contact_info_name);
+        account= (TextView) findViewById(R.id.contact_info_account);
+        tip= (TextView) findViewById(R.id.contact_info_tv_tip);
+        deleteBtn= (Button) findViewById(R.id.contact_info_btn_delete);
+        tv_back= (TextView) findViewById(R.id.tv_back_in_contactinfo);
+        tv_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
                 finish();
-                break;
-            default:
-                finish();
-                break;
-        }
-            return super.onOptionsItemSelected(item);
+            }
+        });
     }
 
     public ImageView getIcon() {
