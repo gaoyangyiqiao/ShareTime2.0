8cff6e979840ed464e0edbf6a3fde456bd343692
zfy <1696621890@qq.com>
2015-06-07 14:18:27 +0800

    qw

--- app/src/main/java/turingmachine/com/sharetime20/SignupFragment.java
+++ app/src/main/java/turingmachine/com/sharetime20/SignupFragment.java
@@ -1,5 +1,6 @@
 package turingmachine.com.sharetime20;
 
+import android.app.AlertDialog;
 import android.app.DatePickerDialog;
 import android.app.Fragment;
 import android.app.TimePickerDialog;
@@ -8,9 +9,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
+import java.sql.Time;
 import java.util.Calendar;
+import java.util.Date;
+
+import netconnection.AddActivity;
+import netconnection.Config;
 
 /**
  * Created by hello on 2015/6/5.
@@ -21,12 +30,20 @@ public class SignupFragment extends Fragment {
     private TextView choosetime2;
     private  TimePickerDialog timePickerDialog;
     private DatePickerDialog datePickerDialog;
+    private EditText editText;
+    private Date cdate;
+    private Time time1;
+    private Time time2;
+    private Date starttime;
+    private Date endtime;
+    int index=1;
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View settingFragment = inflater.inflate(R.layout.activity_signup,container,false);
         return settingFragment;
     }
     public void onViewCreated(View view, Bundle savedInstanceState) {
+
         int hoursofday=Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
         int minute=Calendar.getInstance().get(Calendar.MINUTE);
         int year=Calendar.getInstance().get(Calendar.YEAR);
@@ -35,27 +52,80 @@ public class SignupFragment extends Fragment {
         choosedate=(Button)getActivity().findViewById(R.id.choosedate);
         choosetime1=(TextView)getActivity().findViewById(R.id.choosetime1);
         choosetime2=(TextView)getActivity().findViewById(R.id.choosetime2);
-        timePickerDialog=new TimePickerDialog(getActivity(),null, hoursofday,minute, true);
-        datePickerDialog=new DatePickerDialog(getActivity(),null,year,month,day);
+        timePickerDialog=new TimePickerDialog(getActivity(),new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                if(index==1) {
+                    starttime.setHours(hourOfDay);
+                    starttime.setMinutes(minute);
+choosetime1.setText(hourOfDay+":"+minute);
+                }
+                else{
+                    endtime.setHours(hourOfDay);
+                    endtime.setMinutes(minute);
+                    choosetime2.setText(hourOfDay+":"+minute);
+                }
+            }
+        }, hoursofday,minute, true);
+        datePickerDialog=new DatePickerDialog(getActivity(),new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                choosedate.setText("日期："+(year - 1900)+ "/"+monthOfYear+"/"+ dayOfMonth);
+                cdate=new Date(year - 1900, monthOfYear, dayOfMonth);
+                starttime=new Date(year - 1900, monthOfYear, dayOfMonth);
+                endtime=new Date(year - 1900, monthOfYear, dayOfMonth);
+            }
+        },year,month,day);
+        editText=(EditText)getActivity().findViewById(R.id.activity_get);
+        Button button=(Button)getActivity().findViewById(R.id.confirm);
         choosedate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 datePickerDialog.show();
+
             }
         });
+
         choosetime1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                index=1;
                 timePickerDialog.show();
+
             }
         });
         choosetime2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                index=2;
                 timePickerDialog.show();
 
             }
         });
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String content="";
+                content=editText.getText().toString();
+                if(starttime!=null&&endtime!=null) {
+                    AddActivity addActivity = new AddActivity(Config.getCachedStudentId(getActivity()), content, content, starttime.toString(), endtime.toString(), 0);
+                    LayoutInflater li = LayoutInflater.from(getActivity());
+                    View view = li.inflate(R.layout.activity_add_event, null);
+                    new AlertDialog.Builder(getActivity()).setPositiveButton("确定", null)
+                            .setTitle("新建完成").show();
+
+                }
+                else{
+                    LayoutInflater li = LayoutInflater.from(getActivity());
+                    View view = li.inflate(R.layout.activity_add_event, null);
+                    new AlertDialog.Builder(getActivity()).setPositiveButton("确定", null)
+                            .setTitle("新建没有完成").show();
+                }
+
+
+
+            }
+        });
+
     }
 }
