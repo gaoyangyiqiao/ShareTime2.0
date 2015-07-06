781a9606073fc457c1c101eb5f6679052186a552
gaoyangyiqiao <gyyq1994@gmail.com>
2015-07-07 06:01:45 +0800

    fileoperator

--- app/src/main/java/fileOperator/MatchRecordListFileOperator.java
+++ app/src/main/java/fileOperator/MatchRecordListFileOperator.java
@@ -1,7 +1,9 @@
 package fileOperator;
 
 import android.app.Activity;
+import android.os.Environment;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -9,71 +11,122 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
 import po.MatchRecordListPO;
+import po.MatchRecordPO;
 
 /**
  * Created by gaoyang on 15/7/7.
  */
 public class MatchRecordListFileOperator {
 
-    private void saveObject(MatchRecordListPO matchRecordListPO,String name,Activity activity){
-        FileOutputStream fos = null;
-        ObjectOutputStream oos = null;
+    private FileInputStream fileInputStream;
+    private FileOutputStream fileOutputStream;
+    private ObjectOutputStream objectOutputStream;
+    private ObjectInputStream objectInputStream;
+
+    public MatchRecordListFileOperator(){
+    }
+
+    private void saveObject(MatchRecordListPO matchRecordListPO,String filename){
         try {
-            fos = activity.openFileOutput(name, activity.MODE_PRIVATE);
-            oos = new ObjectOutputStream(fos);
-            oos.writeObject(matchRecordListPO);
+            //存入数据
+            File file = new File(Environment.getExternalStorageDirectory().toString() + File.separator +"ContactBackup"+File.separator +filename);
+            if (!file.getParentFile().exists()) {
+                file.getParentFile().mkdirs();
+            }
+
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+
+            fileOutputStream= new FileOutputStream(file.toString());
+            objectOutputStream= new ObjectOutputStream(fileOutputStream);
+            objectOutputStream.writeObject(matchRecordListPO);
+
         } catch (Exception e) {
-            e.printStackTrace();
-            //这里是保存文件产生异常
-        } finally {
-            if (fos != null){
+            // TODO: handle exception
+        }finally{
+            if (objectOutputStream!=null) {
                 try {
-                    fos.close();
+                    objectOutputStream.close();
                 } catch (IOException e) {
-                    //fos流关闭异常
                     e.printStackTrace();
                 }
             }
-            if (oos != null){
+            if (fileOutputStream!=null) {
                 try {
-                    oos.close();
+                    fileOutputStream.close();
                 } catch (IOException e) {
-                    //oos流关闭异常
+                    e.printStackTrace();
+                }
+            }
+            if (objectInputStream!=null) {
+                try {
+                    objectInputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (fileInputStream!=null) {
+                try {
+                    fileInputStream.close();
+                } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
         }
     }
 
-    private MatchRecordListPO getObject(String name,Activity activity){
-        FileInputStream fis = null;
-        ObjectInputStream ois = null;
+    private MatchRecordListPO getObject(String filename) {
+        MatchRecordListPO matchRecordListPO=new MatchRecordListPO();
         try {
-            fis = activity.openFileInput(name);
-            ois = new ObjectInputStream(fis);
-            return (MatchRecordListPO)(ois.readObject());
+            File file = new File(Environment.getExternalStorageDirectory().toString() + File.separator + "ContactBackup" + File.separator + filename);
+            if (!file.getParentFile().exists()) {
+                file.getParentFile().mkdirs();
+            }
+
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+
+
+            //取出数据
+            fileInputStream = new FileInputStream(file.toString());
+            objectInputStream = new ObjectInputStream(fileInputStream);
+            matchRecordListPO = (MatchRecordListPO) objectInputStream.readObject();
+
+
         } catch (Exception e) {
-            e.printStackTrace();
-            //这里是读取文件产生异常
+            // TODO: handle exception
         } finally {
-            if (fis != null){
+            if (objectOutputStream != null) {
                 try {
-                    fis.close();
+                    objectOutputStream.close();
                 } catch (IOException e) {
-                    //fis流关闭异常
                     e.printStackTrace();
                 }
             }
-            if (ois != null){
+            if (fileOutputStream != null) {
                 try {
-                    ois.close();
+                    fileOutputStream.close();
                 } catch (IOException e) {
-                    //ois流关闭异常
+                    e.printStackTrace();
+                }
+            }
+            if (objectInputStream != null) {
+                try {
+                    objectInputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (fileInputStream != null) {
+                try {
+                    fileInputStream.close();
+                } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
         }
-        //读取产生异常，返回null
-        return null;
+        return matchRecordListPO;
     }
 }
